8bace51c9a0ab0440f9dbd850e37668848e73d6b
theagni0070 <theagni0070@gmail.com>
2022-09-28 10:07:44 +0530

    project created

---
+++ Covid_Vaccination_Tracker/src/main/java/com/covid/CovidVaccinationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccinationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidVaccinationTrackerApplication.class, args);
+	}
+
+}
