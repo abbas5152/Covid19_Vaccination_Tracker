88a4d3fc3379949b8bacbd6d97ca9dcbddc74486
akashsinghdto55 <101391984+akashsinghdto55@users.noreply.github.com>
2022-09-30 22:26:42 +0530

    VacCenterService

---
+++ Covid_Vaccination_Tracker/src/main/java/com/covid/exception/VaccineCenterException.java
@@ -0,0 +1,14 @@
+package com.covid.exception;
+
+public class VaccineCenterException extends RuntimeException{
+	
+	public VaccineCenterException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public VaccineCenterException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
