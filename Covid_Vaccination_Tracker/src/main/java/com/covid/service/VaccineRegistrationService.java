c36d42fde463ea3a1fa54c906fb89cb38e64bf31
Ankit Sah <101566900+Ankit-Sah-Developer@users.noreply.github.com>
2022-10-01 18:25:52 +0530

    Add files via upload

--- Covid_Vaccination_Tracker/src/main/java/com/covid/service/VaccineRegistrationService.java
+++ Covid_Vaccination_Tracker/src/main/java/com/covid/service/VaccineRegistrationService.java
@@ -9,13 +9,13 @@ import com.covid.model.VaccineRegistration;
 public interface VaccineRegistrationService {
            
 	public List<VaccineRegistration> allVaccineRegistration() throws VaccineRegistrationException;
-	public VaccineRegistration addVaccineRegistration(VaccineRegistration registration) throws VaccineRegistrationException;
+	public VaccineRegistration addVaccineRegistration(VaccineRegistration registration, String key) throws VaccineRegistrationException;
 
-	public VaccineRegistration getVaccineRegistration(String mobileNumber) throws VaccineRegistrationException;
+	public VaccineRegistration getVaccineRegistration(String mobileNumber,String key) throws VaccineRegistrationException;
 
-	public List<Member> getAllMember(String mobileNumber) ;
+	public List<Member> getAllMember(String mobileNumber,String key) throws VaccineRegistrationException ;
 
-	public VaccineRegistration updateVaccineRegistration(VaccineRegistration registration) throws VaccineRegistrationException;
+	public VaccineRegistration updateVaccineRegistration(VaccineRegistration registration,String key) throws VaccineRegistrationException;
 
-	  public boolean deleteVaccineRegistration(VaccineRegistration registration) throws VaccineRegistrationException;
+	  public boolean deleteVaccineRegistration(VaccineRegistration registration,String key) throws VaccineRegistrationException;
 }   
