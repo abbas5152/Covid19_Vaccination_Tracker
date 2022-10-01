36f0b4dc5293703b88fa7d7cfe5b1e1d5e9d92da
Abbas Ghaniwala <abbasghaniwala92@gmail.com>
2022-10-01 23:40:37 +0530

    Delete demo directory

--- demo/covid/repo/VaccineCountDao.java
+++
@@ -1,9 +0,0 @@
-package com.covid.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.covid.model.VaccineCount;
-
-public interface VaccineCountDao extends JpaRepository<VaccineCount, Integer>{
-
-}
