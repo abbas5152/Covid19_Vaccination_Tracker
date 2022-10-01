8805bef0e166ddae8128a97f08dc1fff64a5dbba
Shubham Agnihotri <theagni0070@gmail.com>
2022-10-01 18:31:15 +0530

    Update VaccinationCenter.java

--- Covid_Vaccination_Tracker/src/main/java/com/covid/model/VaccinationCenter.java
+++ Covid_Vaccination_Tracker/src/main/java/com/covid/model/VaccinationCenter.java
@@ -26,11 +26,26 @@ public class VaccinationCenter {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer centerId;
-	private String centerName;
-	private String address;
+	@NotNull(message = "Center name can not be null")
+	@NotBlank(message = "Center name is Mandatory")
+	private String centername;
+
+	@NotNull(message = "City can not be null")
+	@NotBlank(message = "City name is Mandatory")
 	private String city;
+
+	@NotNull(message = "Address can not be null")
+	@NotBlank(message = "Address is Mandatory")
+	private String address;
+
+	@NotNull(message = "State can not be null")
+	@NotBlank(message = "State name is Mandatory")
 	private String state;
-	private String pinCode;
+
+	@NotNull(message = "Pincode can not be null")
+	@NotBlank(message = "Pincode is Mandatory")
+	@Size(min = 6, max = 8)
+	private String pincode;
 	
 	@JsonIgnore
 	@ManyToOne(cascade = CascadeType.ALL)
