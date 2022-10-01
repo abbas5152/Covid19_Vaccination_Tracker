91d0a200cac64d11ed4b4c29c706346960b8ec76
Gagan Patil <101593580+gaganpatil1995@users.noreply.github.com>
2022-10-01 21:31:52 +0530

    admincustomerException

---
+++ Covid_Vaccination_Tracker/src/main/java/com/covid/exception/AdminException.java
@@ -0,0 +1,13 @@
+package com.covid.exception;
+
+public class AdminException extends Exception {
+
+	public AdminException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public AdminException(String message) {
+		super(message) ;
+	}
+	
+}
