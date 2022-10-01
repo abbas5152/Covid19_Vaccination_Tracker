1105171cc8a0d00b5708384daaa4b59901d9d8a3
Gagan Patil <101593580+gaganpatil1995@users.noreply.github.com>
2022-10-01 22:30:05 +0530

    Add files via upload

--- Covid_Vaccination_Tracker/src/main/java/com/covid/repo/AppointmentDao.java
+++ Covid_Vaccination_Tracker/src/main/java/com/covid/repo/AppointmentDao.java
@@ -1,12 +1,13 @@
 package com.covid.repo;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.covid.model.Appointment;
-import com.covid.model.Member;
 
 public interface AppointmentDao extends JpaRepository<Appointment, Integer> {
-  public List<Appointment> findByMember(Member member);
+
+	Optional<Appointment> findById(Long bookingId);
+
 }
