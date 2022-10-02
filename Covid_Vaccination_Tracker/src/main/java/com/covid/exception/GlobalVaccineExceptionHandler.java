8866c208a9efae54fd804495a1bd86e6b2da178d
Gagan Patil <101593580+gaganpatil1995@users.noreply.github.com>
2022-10-02 19:47:03 +0530

    Add files via upload

--- Covid_Vaccination_Tracker/src/main/java/com/covid/exception/GlobalVaccineExceptionHandler.java
+++ Covid_Vaccination_Tracker/src/main/java/com/covid/exception/GlobalVaccineExceptionHandler.java
@@ -2,8 +2,17 @@ package com.covid.exception;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.RollbackException;
+import javax.validation.ConstraintViolationException;
+
+import javax.persistence.RollbackException;
+import javax.validation.ConstraintViolationException ;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -37,6 +46,31 @@ public class GlobalVaccineExceptionHandler {
 	    	        
 	    	        return new ResponseEntity<MyErrorDetails>(mrd, HttpStatus.BAD_REQUEST);
 	    }
+	
+	@ExceptionHandler(AdminException.class)
+    public ResponseEntity<MyErrorDetails> globalExceptionHandler(AdminException ve, WebRequest wr){
+    	
+    	        MyErrorDetails mrd= new MyErrorDetails();
+    	        		
+    	        mrd.setTimestap(LocalDateTime.now());
+    	        mrd.setMessage(ve.getMessage());
+    	        mrd.setDetails(wr.getDescription(false));
+    	        
+    	        return new ResponseEntity<MyErrorDetails>(mrd, HttpStatus.BAD_REQUEST);
+    }
+	
+	
+	@ExceptionHandler(CustomerException.class)
+    public ResponseEntity<MyErrorDetails> globalExceptionHandler(CustomerException ve, WebRequest wr){
+    	
+    	        MyErrorDetails mrd= new MyErrorDetails();
+    	        		
+    	        mrd.setTimestap(LocalDateTime.now());
+    	        mrd.setMessage(ve.getMessage());
+    	        mrd.setDetails(wr.getDescription(false));
+    	        
+    	        return new ResponseEntity<MyErrorDetails>(mrd, HttpStatus.BAD_REQUEST);
+    }
 	
 	@ExceptionHandler(IdCardNotFoundException.class)
     public ResponseEntity<MyErrorDetails> globalExceptionHandler(IdCardNotFoundException ve, WebRequest wr){
@@ -183,6 +217,22 @@ public class GlobalVaccineExceptionHandler {
 		    return new ResponseEntity<MyErrorDetails>(mrd, HttpStatus.BAD_REQUEST);
 		    
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<MyErrorDetails> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException manv,
+			WebRequest wr) {
+		String message = manv.getBindingResult().getFieldError().getDefaultMessage();
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(), message, wr.getDescription(false));
+		return new ResponseEntity<>(err, HttpStatus.FORBIDDEN);
+	}
+
+	@ExceptionHandler(RollbackException.class)
+	public ResponseEntity<MyErrorDetails> handleRollbackException(Exception exp, WebRequest req) {
+		MyErrorDetails err = new MyErrorDetails(LocalDateTime.now(),
+				"Improper arguments passed in jason. Validation failed", req.getDescription(false));
+
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<MyErrorDetails> SendExceptionMessage(Exception e, WebRequest wr){
 		
@@ -195,6 +245,11 @@ public class GlobalVaccineExceptionHandler {
 		    return new ResponseEntity<MyErrorDetails>(mrd, HttpStatus.BAD_REQUEST);
 		    
 	}
+	
+	
+
+	
+	
 	@ExceptionHandler(NoHandlerFoundException.class)
 	public ResponseEntity<MyErrorDetails> SendExceptionMessage(NoHandlerFoundException e, WebRequest wr){
 		
