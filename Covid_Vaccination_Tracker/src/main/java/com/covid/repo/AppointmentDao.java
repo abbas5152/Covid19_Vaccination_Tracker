3a664f6fb66d3d3a353ff6520a09fed4595f105d
Gagan Patil <101593580+gaganpatil1995@users.noreply.github.com>
2022-10-02 19:50:59 +0530

    appointDao

--- Covid_Vaccination_Tracker/src/main/java/com/covid/repo/AppointmentDao.java
+++ Covid_Vaccination_Tracker/src/main/java/com/covid/repo/AppointmentDao.java
@@ -4,14 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.covid.model.Appointment;
 import com.covid.model.Member;
 
-public interface AppointmentDao extends JpaRepository<Appointment, Integer> {
+@Repository
+public interface AppointmentDao extends JpaRepository<Appointment, Long> {
 	
 	
   public List<Appointment> findByMember(Member member);
 
-    public Optional<Appointment> findByBookingId(Long bookingId);
-}
+  Optional<Appointment> findByBookingId(Long bookingId);
+  }
