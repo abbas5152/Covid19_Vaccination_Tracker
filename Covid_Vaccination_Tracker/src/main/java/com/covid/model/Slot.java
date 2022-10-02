7a0d2aa60a971fdbfa8ddfe2f967cc25bd662c9d
Shubham Agnihotri <theagni0070@gmail.com>
2022-10-02 17:19:34 +0530

    Delete Covid_Vaccination_Tracker directory

--- Covid_Vaccination_Tracker/src/main/java/com/covid/model/Slot.java
+++
@@ -1,7 +0,0 @@
-package com.covid.model;
-
-public enum Slot {
-	
-	SLOT1,SLOT2,SLOT3,SLOT4,SLOT5
-	
-}
--- Covid_Vaccination_Tracker/src/main/java/com/covid/repo/VaccineRegistrationDao.java
+++
@@ -1,9 +0,0 @@
-package com.covid.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.covid.model.VaccineRegistration;
-
-public interface VaccineRegistrationDao extends JpaRepository<VaccineRegistration, String>{
-
-}
